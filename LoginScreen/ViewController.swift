734f7fe67f84e904fb2c69df719c4323b0fdfddf
Gali Srikanth <GaliSrikanth@gali-srikanth-mba.local>
2018-07-30 23:49:10 +0530

    Login screen UI done using autolayouts

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Gali Srikanth on 30/07/18.
+//  Copyright © 2018 Gs19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
