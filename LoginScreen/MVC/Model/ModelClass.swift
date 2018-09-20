0e033d47b85afbbd3e4b42efd3a8296533bc5abb
Gali Srikanth <GaliSrikanth@gali-srikanth-mba.local>
2018-09-20 08:37:54 +0530

    Few issues fixed

---
+++ LoginScreen/MVC/Model/ModelClass.swift
@@ -0,0 +1,12 @@
+//
+//  ModelClass.swift
+//  LoginScreen
+//
+//  Created by Gali Srikanth on 30/07/18.
+//  Copyright © 2018 Gs19. All rights reserved.
+//
+
+import UIKit
+
+class ModelClass: NSObject {
+}
